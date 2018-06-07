772259d0d90ebfe268b2a230dc767fd233fda3fe
Andrew Leitsius <leitsius@gmail.com>
2018-06-07 18:36:28 +0300

    Add LOT_PREFIX

--- openprocurement/configurator/ea2/includeme.py
+++ openprocurement/configurator/ea2/includeme.py
@@ -5,7 +5,8 @@ from openprocurement.api.configurator import Configurator
 
 config = {
     'AUCTION_PREFIX': 'UA-PS',
-    'ASSET_PREFIX': 'UA-AR-P'
+    'ASSET_PREFIX': 'UA-AR-P',
+    'LOT_PREFIX': 'UA-LR-SSP'
 }
 
 def includeme(*args, **kwargs):
